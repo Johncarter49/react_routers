18d7f3ef72ec118f341aba2a2a7d53c11eb170ef
John Carter <agpakali@gmail.com>
2021-09-16 13:57:42 +0200

    react_routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Header from './components/Header'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
+import Post from './pages/Post';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router forceRefresh>
+      <div className='App'>
+        <Header />
+      </div>
+      <Switch>
+        <Route path='/about' component={About} />
+        <Route path='/profile' component={Profile} />
+        <Route exact path='/' component={Home} />
+        <Route path='/post' component={Post}/>
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Post.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useParams, useLocation } from 'react-router-dom'
+
+const Post = () => {
+
+    const { id } = useParams()
+
+    const item = new URLSearchParams(useLocation().search);
+
+    return (
+        <div>
+            <h2>Post id: {id}</h2>
+            <h2> {item.get('name')} </h2>
+            <h2> {item.get('second')} </h2>
+        </div>
+    )
+}
+
+export default Post
